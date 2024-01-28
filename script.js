a4c1242c680724a98ef2203a5d6929c6a77d1323
xghost2021 <rafaeldias44k@gmail.com>
2024-01-28 02:05:52 -0300

    fix a lot problems

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const Pesquisar = document.querySelector("form");
 const apiKey ="c388e102";
 const lista = document.querySelector("div.filmes");
 const pagina = document.querySelector("main");
+var contador = 0;
+var maximoElementos = 1;
 
 Pesquisar.onsubmit = (ev) =>{
     ev.preventDefault();
@@ -15,16 +17,21 @@ Pesquisar.onsubmit = (ev) =>{
     fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${apiKey}`)
         .then(result=>result.json())
         .then(json => carregaLista(json));
-}
+};
 
 const carregaLista = (json) => {
-    
 
     // Verifica se json.Search está definido e é uma array
     if (!json.Search || !Array.isArray(json.Search)) {
-        alert('Nenhum filme encontrado');
+        if(contador < maximoElementos){
+        var altura2 = document.createElement('div');
+        altura2.style.height="100vh";
+        pagina.appendChild(altura2);
+        contador++;
+        }else{
+        }
         return;
-    }
+    };
 
     json.Search.forEach(element => {
         console.log(element);
@@ -33,19 +40,21 @@ const carregaLista = (json) => {
         item.classList.add("item");
         item.innerHTML = `<img src="${element.Poster}" /><div class="text"><h2>${element.Title}</h2></div>`;
         lista.appendChild(item);
+        setTimeout(() => {
+            item.classList.add("show");
+        }, 50);
     });
-}
+};
 const limparLista = () => {
     // Limpa os itens da lista removendo todos os filhos
     while (lista.firstChild) {
         lista.removeChild(lista.firstChild);
-    }
-}
+    };
+};
 const Altura = () =>{
-    if (pagina.style.height= "100vh"){
-        pagina.style.height="auto"
-    }else{
-        pagina.style.height= "100vh"
-    }
-}
-
+   switch (true) {
+    case(pagina.style.height= "auto"):
+    pagina.style.height="100vh";
+    break;
+ }
+};
